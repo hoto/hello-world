2aab7e0b22a3b75f4c874697f34252ef1eb169c7
Andrzej Rehmann <andrzej.rehmann@motorolasolutions.com>
2016-04-26 20:43:40 +0200

    working get localhost:3000

--- app.js
+++ app.js
@@ -3,7 +3,19 @@ const app = express()
 
 const HOSTNAME = process.env.HOSTNAME
 const PORT = process.env.PORT || 3000
+const NODE_ENV = process.env.NODE_ENV
 
-app.configure(() => app.use(express.bodyParser()))
-app.get('/', (req, res) => res.send(`Hello, hostname: ${HOSTNAME}`))
-app.listen(PORT, console.log(`Listening on port ${PORT}`))
+app.use((req, res, next) => {
+  console.log('Got request...')
+  next()
+})
+
+app.get('/', (req, res) => res.json({
+    hostname: HOSTNAME,
+    nodeEnv: NODE_ENV,
+    date: new Date(),
+    repo: 'hoto/hello-world',
+    type: 'docker'
+}))
+
+app.listen(PORT, console.log(`Listening on port ${PORT}, current time: `, new Date()))
