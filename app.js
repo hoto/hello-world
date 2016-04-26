0cbea2cbbfe56dc23e1f617aa8aab3f1f96b7d8f
Andrzej Rehmann <andrzej.rehmann@motorolasolutions.com>
2016-04-26 20:16:49 +0200

    docker ignore

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const HOSTNAME = process.env.HOSTNAME
-const PORT = process.evn.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.configure(() => app.use(express.bodyParser()))
 app.get('/', (req, res) => res.send(`Hello, hostname: ${HOSTNAME}`))
